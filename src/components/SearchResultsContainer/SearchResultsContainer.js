7b3349c64f20a39b1f0dbd9f8255a01e0866a7cf
Rod Davis <rod.davis@glplusllc.com>
2022-10-12 22:21:30 -0400

    feature/integrate-search-with-yelp-fusion -- code cleanup of console warnings

--- src/components/SearchResultsContainer/SearchResultsContainer.js
+++ src/components/SearchResultsContainer/SearchResultsContainer.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useContext} from "react";
+import React, {useEffect, useContext} from "react";
 import axios from "axios";
 import {AppContext} from '../../App';
 
@@ -6,37 +6,37 @@ import "./SearchResultsContainer.css";
 
 const SearchResultsContainer = () => {
     const {
-        currentLocation
+        currentLocation,
     } = useContext(AppContext);
 
     console.log('currentLocation', currentLocation);
 
-    const getIceCreamShops = async (location) => {
-        let yelpURL = `/v3/businesses/search`;
-
-        const options = {
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`
-            },
-            params : {
-                term: 'ice cream',
-                location: currentLocation
-            }
-        };
-
-        await axios.get(yelpURL, options)
-            .then((response) => {
-                console.log('response', response);
-            })
-            .catch((err) => {
-                console.log('error', err)
-            })
-    }
-
     useEffect(() => {
+        const getIceCreamShops = async (location) => {
+            let yelpURL = `/v3/businesses/search`;
+
+            const options = {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`
+                },
+                params : {
+                    term: 'ice cream',
+                    location: currentLocation
+                }
+            };
+
+            await axios.get(yelpURL, options)
+                .then((response) => {
+                    console.log('response', response);
+                })
+                .catch((err) => {
+                    console.log('error', err)
+                })
+        }
+
         getIceCreamShops(currentLocation);
-    }, [])
+    }, [currentLocation])
 
     return (
         <div className="results-container">
